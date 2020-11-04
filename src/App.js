859b5a9b1c772caea23f54a5fca711e4a7650f6a
pramitha1166 <63492955+pramitha1166@users.noreply.github.com>
2020-11-05 00:07:53 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import 'boostrap/package.json';
+import {BrowserRouter as Router,Switch,Route, Link} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Create from './component/create.component';
+import Edit from './component/edit.component';
+import Index from './component/index.component';
+
+class App extends Component {
+  render() {
+    return(
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to={'/'} className="navbar-brand">React CRUD Example</Link>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <Link to={'/'} className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/create'} className="nav-link">Create</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+        <Switch>
+          <Route exact path='/create' component={Create}/>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/edit.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Edit extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h2>Edit</h2>
+            </div>
+        );
+    }
+}
